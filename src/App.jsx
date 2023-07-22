9a082d9c232ed83804288cb5def2f1bf89441dab
KshitijV97 <writetokshitij@gmail.com>
2023-07-22 19:32:16 +0530

    Added basic css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Column from "./components/Column";
 
 function App() {
 	return (
-		<>
-			<div>App</div>
-		</>
+		<div className='App'>
+			<Column state='PLANNED' />
+			<Column state='ONGOING' />
+			<Column state='DONE' />
+		</div>
 	);
 }
 
