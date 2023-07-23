a74099507eacaee27b5b28e84136c02a39c55eab
KshitijV97 <writetokshitij@gmail.com>
2023-07-23 08:35:10 +0530

    Added Add button and its styles

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,11 +1,24 @@
+import { useStore } from "../store";
+import { shallow } from "zustand/shallow";
 import "./Column.css";
 import Task from "./Task";
 
 export default function Column({ state }) {
+	const tasks = useStore(
+		(store) => store.tasks.filter((task) => task.state === state),
+		shallow
+	);
+
 	return (
 		<div className='column'>
-			<p>{state}</p>
-			<Task title='TODO' />
+			<div className='titleWrapper'>
+				<p>{state}</p>
+				<button>Add</button>
+			</div>
+
+			{tasks.map((task) => (
+				<Task title={task.title} key={task.title} />
+			))}
 		</div>
 	);
 }
