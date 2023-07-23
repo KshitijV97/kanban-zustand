ea705e4565a1c4a6841d44df462b3eb0b7c18408
KshitijV97 <writetokshitij@gmail.com>
2023-07-23 08:35:31 +0530

    Taking task from zustand

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,15 +1,17 @@
 import "./Task.css";
 import classNames from "classnames";
-
-const STATUS = "DONE";
+import { useStore } from "../store";
 
 export default function Task({ title }) {
+	const task = useStore((store) =>
+		store.tasks.find((task) => task.title === title)
+	);
 	return (
 		<div className='task'>
-			<div>{title}</div>
+			<div>{task.title}</div>
 			<div className='bottomWrapper'>
 				<div></div>
-				<div className={classNames("status", STATUS)}>{STATUS}</div>
+				<div className={classNames("status", task.state)}>{task.state}</div>
 			</div>
 		</div>
 	);
